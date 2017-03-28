81a8dd683716cd01ea167f4e27e8b62a24f3cd3f
Katy <ihatetoast@gmail.com>
2017-03-27 23:07:56 -0500

    started fcn with user and farmer ted choice

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ Why am I doing this? It's too simple to be bothered with, right?
 * ES6
 * Sketch 
 * So neener neener!
+
+
+---
+## Notes:
+### Rules:
+* egg beats feathers 
+* feathers beats beak 
+* beak beats egg
+
+### Matching illustrations:
+* egg lying atop feathers
+* feathers stuck in beak like it annoys the chicken
+* beak poking out of egg
+
--- main.js
+++ main.js
@@ -1,53 +1,72 @@
 // Take old RPS game and change to ES6
 // make this more visual and fun. pretend you're creating for a child.
-
+//it'll be user v farmer ted 
 
 //below is old. straight copy paste. fix soon for this project
 
 //get rid of prompt. let user click on an image.
-var userChoice = prompt("Do you choose rock, paper or scissors?");
+// let userchoice = tbd
+// let farmerTedChoice = tbd
+
+//testing:
+const btnClk = document.getElementById("farmerTed");
+const userChoice = document.querySelectorAll('.userChoice');
 
 
-//computer's choice
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-    computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-    computerChoice = "paper";
-} else {
-    computerChoice = "scissors";
-} 
+//computer's (Farmer Ted's) choice
+let farmerTedChoice;
+btnClk.addEventListener('click', ()=>{
+    function getRando() {
+        return (Math.floor(Math.random() * 3) + 1);
+    }
+    farmerTedChoice = getRando();
+    console.log(farmerTedChoice);
+    if(farmerTedChoice === 3){
+      console.log("Farmer Ted chose egg.");
+    } else if(farmerTedChoice === 2){
+      console.log("Farmer Ted chose beak.");
+    } else {
+      console.log("Farmer Ted chose feathers.")
+    }
+});
 
+userChoice.forEach(userChoice => userChoice.addEventListener('click', function(){
+    console.log("userbutton clicked: "+this.dataset.choice);
+}));
+
+// egg beats feathers 
+// feathers beats beak 
+// beak beats egg
 //compare fcn
-//undo old. rewrite in ES6
-var compare = function(choice1, choice2) {
-    if (choice1 === choice2) {
-        return "The result is a tie!";
-    }
-    else if (choice1 === "rock"){
-        if (choice2 === "scissors"){
-            return "rock wins";
-        }
-        else {
-            return "paper wins";
-        }
-    }
-    else if (choice1 === "paper"){
-        if (choice2 === "rock"){
-            return "paper wins";
-        }
-        else {
-            return "scissors wins";
-        }
-    }
-    else if (choice1 === "scissors"){
-        if (choice2 === "rock"){
-            return "rock wins";
-        }
-    else {
-        return "scissors wins";
-    }
-}
+//undo old. rewrite in ES6, c
+// let compare = (choice1, choice2) => {
+//     if (choice1 === choice2) {
+//         return "The result is a tie!";
+//     }
+//     else if (choice1 === "egg"){
+//         if (choice2 === "feathers"){
+//             return "Egg wins.";
+//         }
+//         else {
+//             return "Beak wins.";
+//         }
+//     }
+//     else if (choice1 === "feathers"){
+//         if (choice2 === "beak"){
+//             return "Feathers win.";
+//         }
+//         else {
+//             return "Egg wins.";
+//         }
+//     }
+//     else if (choice1 === "beak"){
+//         if (choice2 === "egg"){
+//             return "Beak wins.";
+//         }
+//     else {
+//         return "Feathers win.";
+//     }
+// }
 
-};
-compare(userChoice, computerChoice);
+// };
+// compare(userChoice, farmerTedChoice);
